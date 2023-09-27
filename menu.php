f67e12e8605b9fc4b31c8bbeb74f9917a0729c58
DarkCoderX1323 <rayvargas1323@gmail.com>
2023-09-27 16:46:59 -0500

    Update

---
+++ menu.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Editorial Berlin - Tienda en Línea</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <img src="https://editorialberlin.com/wp-content/uploads/2022/02/logo-berlin-1-e1629321593429.png" alt="Logo de Editorial Berlin">
+            </div>
+            <ul class="menu">
+                <li><a href="#">Promociones</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Mi cuenta</a></li>
+                <li><a href="#"><img src="carrito.png" alt="Carrito de compras"></a></li>
+            </ul>
+        </nav>
+        <div class="banner">
+            <!-- Aquí puedes agregar las imágenes para el banner en bucle -->
+            <img src="imagen1.jpg" alt="Imagen 1">
+            <img src="imagen2.jpg" alt="Imagen 2">
+            <img src="imagen3.jpg" alt="Imagen 3">
+        </div>
+    </header>
+
+    <main>
+        <section class="product-list">
+            <!-- Lista de productos -->
+            <div class="product">
+                <img src="producto1.jpg" alt="Producto 1">
+                <h2>Producto 1</h2>
+                <p class="price">$99.99</p>
+                <button>Comprar</button>
+            </div>
+
+            <div class="product">
+                <img src="producto2.jpg" alt="Producto 2">
+                <h2>Producto 2</h2>
+                <p class="price">$79.99</p>
+                <button>Comprar</button>
+            </div>
+
+            <!-- Puedes agregar más productos aquí -->
+        </section>
+    </main>
+
+    <footer>
+        <!-- Pie de página -->
+    </footer>
+</body>
+</html>
